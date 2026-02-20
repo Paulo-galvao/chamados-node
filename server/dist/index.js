6bd4893fd8f72d3db2c1c1fd82dbd818c69869ea
Paulo-galvao <paulogalvaoj21@gmail.com>
2026-02-20 20:32:03 -0300

    update configs

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,7 +1,38 @@
 import express from "express";
+import { prisma } from "./lib/prisma.js";
 const app = express();
-app.get("/", (req, res) => {
-    res.send("Hello from chamados server");
+app.use(express.json());
+app.get("/", async (req, res) => {
+    const requests = await prisma.request.findMany();
+    res.send(requests);
+});
+app.post("/requests", async (req, res) => {
+    const request = await prisma.request.create({
+        data: {
+            title: req.body.title,
+            description: req.body.description,
+            category_id: 1,
+            status_id: 1
+        }
+    });
+    res.status(201).send(request);
+});
+app.post("/categories", async (req, res) => {
+    const { name } = req.body;
+    const request = await prisma.category.create({
+        data: {
+            name: name
+        }
+    });
+    res.status(201).send(request);
+});
+app.post("/status", async (req, res) => {
+    const { name } = req.body;
+    const request = await prisma.status.create({
+        data: {
+            name: name
+        }
+    });
+    res.status(201).send(request);
 });
 app.listen(3000, () => console.log("Server running on port 3000"));
-//# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { prisma } from "./lib/prisma";
+import { prisma } from "./lib/prisma.js";
 
 const app = express();
 app.use(express.json());
